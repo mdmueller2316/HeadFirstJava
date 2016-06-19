fe8b143c09ae02ff0d8edf5d396e171470c8179a
Michael Mueller <michaelm@maildg.com>
2016-06-18 20:51:07 -0500

    Polymorphism Exploration

--- src/InheritanceAndPolymorphism/AnimalList.java
+++ src/InheritanceAndPolymorphism/AnimalList.java
@@ -4,7 +4,7 @@ package InheritanceAndPolymorphism;
  * Created by Michael on 6/18/2016.
  */
 public class AnimalList {
-	private Animal[] animals = new Animal[];
+	private Animal[] animals = new Animal[5];
 	private int nextIndex = 0;
 
 	public void add(Animal a) {
