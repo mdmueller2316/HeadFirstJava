cf7568fd4759c981690b0990efb3ce72738fe8e4
Michael Mueller <michaelm@maildg.com>
2016-06-20 15:44:03 -0500

    Work

---
+++ src/MIDIBeatBox/MusicAppGUI.java
@@ -0,0 +1,18 @@
+package MIDIBeatBox;/**
+ * Created by mm40 on 6/20/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MusicAppGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
